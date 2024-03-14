b86a3057d66a12d0a6df300fe726a703401dd6ae
Solractys <carloseduardosilvasouzadelima@gmail.com>
2024-03-14 01:03:00 +0000

    HourlyRate

--- UI/ui/src/app/components/HourlyRateForm.tsx
+++ UI/ui/src/app/components/HourlyRateForm.tsx
@@ -10,24 +10,27 @@ const HourlyRateForm: React.FC = () => {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(hourlyRate)
+      console.log(hourlyRate)
   };
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Qual o seu valor por hora de trabalho?
-          <input
-            type="number"
-            step={0.01}
-            min={0}
-            value={hourlyRate}
-            onChange={handleInputChange}
-          />
-        </label>
-          <button type="submit">Enviar</button>
-      </form>
-    </div>
-  );
+return (
+  <div className="p-4 w-max flex shadow-md rounded-md">
+    <form onSubmit={handleSubmit} className="">
+      <label className="flex flex-col text-lg font-semibold text-slate-800 ">
+        Qual o seu valor por hora de trabalho?
+        <input
+          type="number"
+          step={0.01}
+          min={0}
+          value={hourlyRate}
+          onChange={handleInputChange}
+          className="bg-transparent w-20 my-7 mx-4 rounded-md shadow-sm p-2 "
+        />
+      </label>
+        <div className="flex justify-end items-end">
+      <button className="w-fit rounded-lg font-semibold shadow-lg text-white bg-orange-400 px-4 hover:bg-white transition hover:text-orange-400 " type="submit">Enviar</button>
+      </div>
+    </form>
+  </div>
+);
 }
 export default HourlyRateForm;
--- UI/ui/src/app/page.tsx
+++ UI/ui/src/app/page.tsx
@@ -4,8 +4,8 @@ import Cronometer from "./components/cronometer";
 export default function Home() {
   return (
     <div className="">
-      <h1 className="text-center text-5xl m-10 font-semibold">TimeSquad</h1>
-      <div className="flex">
+      <h1 className="text-center text-5xl m-10 font-semibold">Time<em className="text-orange-400">Squad</em></h1>
+      <div className="flex justify-center align-middle mx-auto gap-8">
         <HourlyRateForm />
         <Cronometer />
       </div>
