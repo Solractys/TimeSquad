1bb602aa0a769f135318f73f6eb4c275789c5f00
Solractys <carloseduardosilvasouzadelima@gmail.com>
2024-03-16 02:07:54 -0300

    test docker

--- UI/ui/src/app/page.tsx
+++ UI/ui/src/app/page.tsx
@@ -2,14 +2,14 @@
 import HourlyRateForm from "./components/HourlyRateForm";
 import Cronometer from "./components/Cronometer";
 import Summary from "./components/Summary";
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-export default function Home() {
+export default function Home(): any {
   const [elapsedTime, setElapsedTime] = useState<number>(0);
   const [hourlyRate, setHourlyRate] = useState<number>(0);
 
   return (
-    <div className="">
+    <div>
       <h1 className="text-center text-5xl m-10 font-semibold">Time<em className="text-orange-400">Squad</em></h1>
       <div className="flex justify-center align-middle mx-auto gap-8">
         <HourlyRateForm setHourlyRate={setHourlyRate} />
